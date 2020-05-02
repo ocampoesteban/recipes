becbbb1d033614661198483641c6564beb0f5d22
steve <estebanocampo21@gmail.com>
2020-05-01 22:38:06 -0300

    Initial Commit

---
+++ src/screens/Auht/AuhtenticationScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Text, 
+    View,
+  } from 'react-native';
+
+export default class AuhtenticationScreen extends React.Component {
+    render() {
+        return (
+          <View>
+            <Text>s2423</Text>
+          </View>
+        );
+      }
+}
